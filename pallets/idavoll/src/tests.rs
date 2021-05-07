5173e03f0ac08b72ebd088ea96b52da5081255a0
jasonberger0 <jasonberger110@gmail.com>
2021-05-07 15:46:11 +0800

    add test for create organization

--- pallets/idavoll/src/tests.rs
+++ pallets/idavoll/src/tests.rs
@@ -4,13 +4,14 @@ use frame_support::{assert_ok, assert_noop};
 use frame_system::RawOrigin;
 
 #[test]
-fn it_works_for_first_organization() {
+fn it_works_for_first_dao() {
 	new_test_ext().execute_with(|| {
 		let c = IdavollModule::counter_of();
 		let org_id = create_new_organization(OWNER.clone(),100);
 		assert_ne!(org_id,u128::MAX);
 		assert_ok!(IdavollModule::deposit_to_origanization(RawOrigin::Signed(A).into(),c,200));
 		assert_eq!(IdavollModule::get_count_members(org_id),1);
+		assert_eq!(IdavollAsset::vault_balance_of(org_id.clone()),Ok(200 as u64));
 
 		set_block_number(1);
 		assert_eq!(get_block_number(),1 as u64);
@@ -28,18 +29,47 @@ fn it_works_for_first_organization() {
 		assert_eq!(IdvBalances::free_balance(RECEIVER.clone()),0 as u64);
 		// vote for the proposal by the same proposal_id
 		assert_ok!(IdavollModule::vote_proposal(RawOrigin::Signed(OWNER.clone()).into(),proposal_id.clone(),80,true));
-		// make sure the RECEIVER has the 'value' balance
+		// make sure the RECEIVER has the 'value' balance and the vault was reduce 'value' balance
 		assert_eq!(IdvBalances::free_balance(RECEIVER.clone()),10 as u64);
+		assert_eq!(IdavollAsset::vault_balance_of(org_id.clone()),Ok(190 as u64));
 	});
 }
 
 #[test]
-fn correct_error_for_none_value() {
+fn it_works_for_create_organization() {
 	new_test_ext().execute_with(|| {
-		// Ensure the expected error is thrown when no value is present.
-		// assert_noop!(
-		// 	IdavollModule::cause_error(Origin::signed(1)),
-		// 	Error::<Test>::NoneValue
-		// );
+
+		for i in 1..10 {
+			let c = IdavollModule::counter_of();
+			let org_id = create_new_organization(i,100*i as u64);
+			assert_ne!(org_id,u128::MAX);
+			let asset_id = IdavollModule::get_token_id_by_oid(org_id.clone()).unwrap();
+			assert_eq!(IdavollModule::get_total_token_by_oid(org_id.clone()),Ok(100*i as u64));
+			assert_ok!(IdavollModule::deposit_to_origanization(RawOrigin::Signed(A).into(),c,10+i as u64));
+			assert_eq!(IdavollModule::get_count_members(org_id),1);
+			assert_eq!(IdavollAsset::vault_balance_of(org_id.clone()),Ok(10+i as u64));
+			assert_eq!(IdavollAsset::free_balance(asset_id.clone(),&i),100*i as u64);
+			assert_eq!(IdavollModule::get_local_balance(org_id.clone()),Ok(10+i as u64));
+		}
+	});
+}
+
+#[test]
+fn it_works_for_add_member() {
+	new_test_ext().execute_with(|| {
+		let mut c = IdavollModule::counter_of();
+		let mut org_id = create_new_organization(OWNER.clone(),100);
+		assert_ne!(org_id,u128::MAX);
+		let mut asset_id = IdavollModule::get_token_id_by_oid(org_id.clone()).unwrap();
+		assert_eq!(IdavollModule::get_total_token_by_oid(org_id.clone()),Ok(100));
+		assert_ok!(IdavollModule::deposit_to_origanization(RawOrigin::Signed(A).into(),c,10));
+		assert_eq!(IdavollModule::get_count_members(org_id),1);
+		assert_eq!(IdavollAsset::vault_balance_of(org_id.clone()),Ok(10));
+		assert_eq!(IdavollAsset::free_balance(asset_id.clone(),&OWNER.clone()),100);
+		assert_eq!(IdavollModule::get_local_balance(org_id.clone()),Ok(10));
+
+		// add members in the organization by org_id
+		
+
 	});
 }
